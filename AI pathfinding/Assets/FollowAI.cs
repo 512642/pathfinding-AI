4ad9fb568c475e65d64b7c02514a194c9f4e42d0
512642 <72399788+512642@users.noreply.github.com>
2021-11-08 14:16:59 +0000

    g

--- AI pathfinding/Assets/FollowAI.cs
+++ AI pathfinding/Assets/FollowAI.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIFollow : MonoBehaviour
+public class FollowAI : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject robot;
+    public Vector3 offset;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = robot.transform.position + offset;
     }
 }
